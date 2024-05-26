a86cfdc7693852008c68b10b8f53d30aca5ded8d
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-05-26 14:59:19 +0200

    Setting up tailwindCSS configuration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePageController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home_page')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomePageController',
+            'controller_name' => 'HomeController',
         ]);
     }
 }
