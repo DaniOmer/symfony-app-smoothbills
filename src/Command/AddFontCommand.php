e627f660d1e094e8237df6be6478c05c0f652aac
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-05-27 11:25:37 +0200

    refactor: separate font addition logic into FontService
    - Moved the logic for adding fonts from the command to a new service, FontService.
    - Updated the AddFontCommand to use FontService for adding fonts.
    - Improved separation of concerns by isolating business logic in the service.
    - Enhanced maintainability and testability of the codebase

--- src/Command/AddFontCommand.php
+++ src/Command/AddFontCommand.php
@@ -10,20 +10,20 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 use App\Entity\Font;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\FontService;
 
 #[AsCommand(
-    name: 'AddFont',
+    name: 'app:AddFont',
     description: 'Adds a font to the font entity',
 )]
 class AddFontCommand extends Command
 {
-    private $entityManager;
+    private $fontService;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(FontService $fontService)
     {
         parent::__construct();
-        $this->entityManager = $entityManager;
+        $this->fontService = $fontService;
     }
 
     protected function configure(): void
@@ -51,36 +51,23 @@ class AddFontCommand extends Command
             return Command::FAILURE;
         }
 
-        $this->insertFont($fonts);
+        foreach ($fonts as $font) {
+            $fontName = $font['name'];
+
+            try {
+                if($this->fontService->addFontIfNotExists($fontName)){
+                    $io->success(sprintf('Font "%s" has been added successfully.', $fontName));
+                }else{
+                    $io->warning(sprintf('Font "%s" already exists.', $fontName));
+                }
+            } catch (\Exception $e) {
+                $io->error(sprintf('An error occured while adding the font "%s".', $fontName));
+                return Command::FAILURE;
+            }
+        }
+
 
         $io->success('Font JSON file processed successfully.');
-
         return Command::SUCCESS;
-    }
-
-    public function insertFont(array $fonts): void
-    {
-        $entityManager = $this->entityManager;
-        $entityManager->beginTransaction();
-        
-        try {
-            foreach($fonts as $font) {
-                $fontName = $font['name'];
-    
-                $existingFont = $entityManager->getRepository(Font::class)->findOneBy(['name' => $fontName]);
-                if(!$existingFont) {
-                    $newFont = new Font();
-                    $newFont->setName($fontName);
-    
-                    $entityManager->persist($newFont);
-                }
-            }
-            
-            $entityManager->flush();
-            $entityManager->commit();
-        } catch (\Exception $e) {
-            $entityManager->rollback();
-            throw $e;
-        }
     }
 }
