53509ba1e4ce3a6e458f4b83fe9e89b92787104a
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-06-08 16:45:15 +0200

    Translate the error message on email exist

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte associé à cette adresse e-mail.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     use TimestampableTrait;
