0aa3f00456f0b6fefbd957c1488436df866b7642
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-06-09 20:00:15 +0200

    Update customer vue worklow by: - updating route naming from app_customer_ to dashboard.customer. - updating templates related to customer

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -12,18 +12,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/customer')]
+#[Route('/dashboard/customer')]
 class CustomerController extends AbstractController
 {
-    #[Route('/', name: 'app_customer_index', methods: ['GET'])]
+    #[Route('/', name: 'dashboard.customer.index', methods: ['GET'])]
     public function index(CustomerRepository $customerRepository): Response
     {
-        return $this->render('customer/index.html.twig', [
+        return $this->render('dashboard/customer/index.html.twig', [
             'customers' => $customerRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_customer_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'dashboard.customer.new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $customer = new Customer();
@@ -44,24 +44,24 @@ class CustomerController extends AbstractController
             
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_customer_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('dashboard.customer.index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('customer/new.html.twig', [
+        return $this->render('dashboard/customer/new.html.twig', [
             'customer' => $customer,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_customer_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'dashboard.customer.show', methods: ['GET'])]
     public function show(Customer $customer): Response
     {
-        return $this->render('customer/show.html.twig', [
+        return $this->render('dashboard/customer/show.html.twig', [
             'customer' => $customer,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_customer_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'dashboard.customer.edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Customer $customer, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(CustomerType::class, $customer);
@@ -70,16 +70,16 @@ class CustomerController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_customer_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('dashboard/dashboard.customer.index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('customer/edit.html.twig', [
+        return $this->render('dashboard/customer/edit.html.twig', [
             'customer' => $customer,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_customer_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'dashboard.customer.delete', methods: ['POST'])]
     public function delete(Request $request, Customer $customer, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$customer->getId(), $request->getPayload()->get('_token'))) {
@@ -87,6 +87,6 @@ class CustomerController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_customer_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('dashboard/dashboard.customer.index', [], Response::HTTP_SEE_OTHER);
     }
 }
