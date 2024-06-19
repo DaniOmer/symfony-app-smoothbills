e65631ae1b21abaef37d423a246baf175e77b2db
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-06-19 15:58:57 +0200

    Setting up User controller for actions related to user and manage user profile update

---
+++ src/Controller/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/dashboard/settings/user')]
+class UserController extends AbstractController
+{
+    // #[Route('/manage', name: 'dashboard.settings.user.manage', methods: ['GET'])]
+    // public function index(UserRepository $userRepository): Response
+    // {
+    //     return $this->render('user/index.html.twig', [
+    //         'users' => $userRepository->findAll(),
+    //     ]);
+    // }
+
+    #[Route('/profile', name: 'dashboard.settings.user.profile', methods: ['GET', 'POST'])]
+    public function updateProfile(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Vos informatiosn ont bien été mis à jour.');
+            return $this->redirectToRoute('dashboard.settings.user.profile', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('dashboard/user/update.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    // #[Route('/invitation', name: 'dashboard.settings.user.invitation', methods: ['GET', 'POST'])]
+    // public function addUser(Request $request, EntityManagerInterface $entityManager): Response
+    // {
+    //     $user = new User();
+    //     $form = $this->createForm(UserType::class, $user);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->persist($user);
+    //         $entityManager->flush();
+
+    //         $this->addFlash('success', 'L\'utilisateur a bien été créer');
+    //         return $this->redirectToRoute('dashboard.settings.user.invitation', [], Response::HTTP_SEE_OTHER);
+    //     }
+
+    //     return $this->render('user/new.html.twig', [
+    //         'user' => $user,
+    //         'form' => $form,
+    //     ]);
+    // }
+
+
+    // #[Route('/{id}', name: 'dashboard.settings.user.delete', methods: ['POST'])]
+    // public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    // {
+    //     if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->get('_token'))) {
+    //         $entityManager->remove($user);
+    //         $entityManager->flush();
+    //     }
+
+    //     return $this->redirectToRoute('dashboard.settings.user.manage', [], Response::HTTP_SEE_OTHER);
+    // }
+}
