d39917a0260cdc8aa49ef5e3006fccc07c7aeda9
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-07-02 04:33:25 +0200

    Implementing static pages.

--- assets/controllers/animation_controller.js
+++ assets/controllers/animation_controller.js
@@ -1,70 +1,38 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["listItems", "images"];
+  static targets = ["listItems", "images"];
 
-  
-    initialize() {
-        this.currentIndex = 0;
-        this.intervalId = null;
-    }
+  initialize() {
+    this.currentIndex = 0;
+  }
 
-    connect() {
-        console.log('Carousel controller connected');
-        this.initializeVisibility();
-        this.startInterval();
-        this.setupListeners();
-    }
+  connect() {
+    this.setupListeners();
+    this.showImage(0);
+  }
 
-    initializeVisibility() {
-        this.imagesTargets.forEach((image, idx) => {
-            image.classList.toggle('content-image', idx === 0);
-            image.classList.toggle('hidden', idx !== 0);
-        });
-    }
+  setupListeners() {
+    this.listItemsTargets.forEach((item, index) => {
+      item.addEventListener("mouseover", () => this.handleMouseOver(index));
+      item.addEventListener("mouseleave", () => this.handleMouseLeave(index));
+    });
+  }
 
-    updateVisibility(index) {
-        this.imagesTargets.forEach((image, idx) => {
-            const shouldBeVisible = idx === index;
-            image.classList.toggle('content-image', shouldBeVisible);
-            image.classList.toggle('hidden', !shouldBeVisible);
-        });
-    }
+  handleMouseOver(index) {
+    this.listItemsTargets.forEach((item, idx) => {
+      item.classList.toggle("custom-list", idx === index);
+    });
+    this.showImage(index);
+  }
 
-    changeActiveItem() {
-        this.listItemsTargets[this.currentIndex].classList.remove('custom-li-class');
-        this.currentIndex = (this.currentIndex + 1) % this.listItemsTargets.length;
-        this.listItemsTargets[this.currentIndex].classList.add('custom-li-class');
-        this.updateVisibility(this.currentIndex);
-    }
+  //   handleMouseLeave(index) {
+  //     this.listItemsTargets[index].classList.remove("custom-list");
+  //   }
 
-    startInterval() {
-        this.intervalId = setInterval(() => this.changeActiveItem(), 3000);
-    }
-
-    stopInterval() {
-        clearInterval(this.intervalId);
-    }
-
-    setupListeners() {
-        this.listItemsTargets.forEach((item, index) => {
-            item.addEventListener('mouseover', () => this.handleMouseOver(index));
-            item.addEventListener('mouseleave', () => this.handleMouseLeave());
-        });
-    }
-
-    handleMouseOver(index) {
-        this.stopInterval();
-        this.listItemsTargets[this.currentIndex].classList.remove('custom-li-class');
-        this.currentIndex = index;
-        this.listItemsTargets[this.currentIndex].classList.add('custom-li-class');
-        this.updateVisibility(this.currentIndex);
-    }
-
-    handleMouseLeave() {
-        this.startInterval();
-        this.listItemsTargets[this.currentIndex].classList.remove('custom-li-class');
-        this.listItemsTargets[this.currentIndex].classList.add('custom-li-class');
-        this.updateVisibility(this.currentIndex);
-    }
+  showImage(index) {
+    this.imagesTargets.forEach((image, idx) => {
+      image.classList.toggle("hidden", idx !== index);
+    });
+  }
 }
