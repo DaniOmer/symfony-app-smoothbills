0b8e202941decd536adacd184fd635be22d2f72c
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-07-02 09:33:36 +0200

    Fix migrations conflict.

--- migrations/Version20240701171150.php
+++ migrations/Version20240701171150.php
@@ -19,14 +19,14 @@ final class Version20240701171150 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE quotation ADD quotation_number VARCHAR(14) NOT NULL');
+        // // this up() migration is auto-generated, please modify it to your needs
+        // $this->addSql('ALTER TABLE quotation ADD quotation_number VARCHAR(14) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE quotation DROP quotation_number');
+        // // this down() migration is auto-generated, please modify it to your needs
+        // $this->addSql('CREATE SCHEMA public');
+        // $this->addSql('ALTER TABLE quotation DROP quotation_number');
     }
 }
--- migrations/Version20240701172302.php
+++ migrations/Version20240701172302.php
@@ -19,20 +19,20 @@ final class Version20240701172302 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE one_time_payment ALTER payment_date DROP NOT NULL');
-        $this->addSql('ALTER TABLE recurring_payment ALTER payment_date DROP NOT NULL');
-        $this->addSql('ALTER TABLE recurring_payment ALTER start_date DROP NOT NULL');
-        $this->addSql('ALTER TABLE recurring_payment ALTER end_date DROP NOT NULL');
+        // // this up() migration is auto-generated, please modify it to your needs
+        // $this->addSql('ALTER TABLE one_time_payment ALTER payment_date DROP NOT NULL');
+        // $this->addSql('ALTER TABLE recurring_payment ALTER payment_date DROP NOT NULL');
+        // $this->addSql('ALTER TABLE recurring_payment ALTER start_date DROP NOT NULL');
+        // $this->addSql('ALTER TABLE recurring_payment ALTER end_date DROP NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE recurring_payment ALTER payment_date SET NOT NULL');
-        $this->addSql('ALTER TABLE recurring_payment ALTER start_date SET NOT NULL');
-        $this->addSql('ALTER TABLE recurring_payment ALTER end_date SET NOT NULL');
-        $this->addSql('ALTER TABLE one_time_payment ALTER payment_date SET NOT NULL');
+        // // this down() migration is auto-generated, please modify it to your needs
+        // $this->addSql('CREATE SCHEMA public');
+        // $this->addSql('ALTER TABLE recurring_payment ALTER payment_date SET NOT NULL');
+        // $this->addSql('ALTER TABLE recurring_payment ALTER start_date SET NOT NULL');
+        // $this->addSql('ALTER TABLE recurring_payment ALTER end_date SET NOT NULL');
+        // $this->addSql('ALTER TABLE one_time_payment ALTER payment_date SET NOT NULL');
     }
 }
